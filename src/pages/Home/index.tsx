49a3f8f95b28938b93b0be94ca75d71fe4e7d41a
Uedson Reis <uedsonreis@SOVBRZNB001033.Corp.sovos.local>
2022-11-08 20:57:32 -0300

    WIP

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -7,14 +7,18 @@ import styles from './styles'
 
 export default function HomePage() {
 
-    const [planets, setPlanets] = React.useState([]);
+    const [planets, setPlanets] = React.useState<any[]>([]);
 
     React.useEffect(() => {
-        service.getPlanets().then(data => setPlanets(data))
+        service.getPlanets().then(data => {
+            console.log(data[0].description)
+            setPlanets(data)
+        })
     }, [])
 
     return (
         <View style={styles.container}>
+            { planets.length < 1 && (<Text>Loading...</Text>) }
             <Text>Solar System Planets: {planets.length}</Text>
         </View>
     )
